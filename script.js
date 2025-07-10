69b4322eb42869a7f820739b52fc77406c410a1c
Prema Latha <120620842+premalatha-sureshbabu@users.noreply.github.com>
2025-07-10 14:28:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+const quotes = [
+  { text: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+  { text: "Your limitation—its only your imagination.", author: "Unknown" },
+  { text: "Push yourself, because no one else is going to do it for you.", author: "Unknown" },
+  { text: "Great things never come from comfort zones.", author: "Unknown" },
+  { text: "Dream it. Wish it. Do it.", author: "Unknown" },
+  { text: "Success doesnt just find you. You have to go out and get it.", author: "Unknown" }
+];
+
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+const button = document.getElementById("new-quote");
+
+button.addEventListener("click", function() {
+  const randomIndex = Math.floor(Math.random() * quotes.length);
+  const randomQuote = quotes[randomIndex];
+  
+  quoteEl.innerText = randomQuote.text;
+  authorEl.innerText = "- " + randomQuote.author;
+});
